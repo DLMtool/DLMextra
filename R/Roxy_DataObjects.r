646d8fd70ec791b48b862416b0c9e29feb008341
Adrian Hordyk <ar.hordyk@gmail.com>
2017-10-17 11:33:09 -0700

    update data

---
+++ R/Roxy_DataObjects.r
@@ -0,0 +1,114 @@
+# This file is automatically built by DataObjectsforDLMextra.r in DLMDev
+# Don't edit by hand!
+# 
+
+#'  testMSE MSE
+#'
+#'  An object of class MSE
+#'
+"testMSE"
+
+
+#'  San_Francisco_Bay_herring_HerringSFB_OM OM
+#'
+#'  An object of class OM
+#'
+"San_Francisco_Bay_herring_HerringSFB_OM"
+
+
+#'  Darnley_Bay_Char_ArcticCharr_OM OM
+#'
+#'  An object of class OM
+#'
+"Darnley_Bay_Char_ArcticCharr_OM"
+
+
+#'  Canary_Rockfish_BC_CanaryRockfishBC_OM OM
+#'
+#'  An object of class OM
+#'
+"Canary_Rockfish_BC_CanaryRockfishBC_OM"
+
+
+#'  AJ_AlmacoJack_OM OM
+#'
+#'  An object of class OM
+#'
+"AJ_AlmacoJack_OM"
+
+
+#'  LS_LaneSnapper_OM OM
+#'
+#'  An object of class OM
+#'
+"LS_LaneSnapper_OM"
+
+
+#'  LAJ_LesserAmberjack_OM OM
+#'
+#'  An object of class OM
+#'
+"LAJ_LesserAmberjack_OM"
+
+
+#'  RD_RedDrum_OM OM
+#'
+#'  An object of class OM
+#'
+"RD_RedDrum_OM"
+
+
+#'  SG_SnowyGrouper_OM OM
+#'
+#'  An object of class OM
+#'
+"SG_SnowyGrouper_OM"
+
+
+#'  SH_SpeckledHind_OM OM
+#'
+#'  An object of class OM
+#'
+"SH_SpeckledHind_OM"
+
+
+#'  WM_Wenchman_OM OM
+#'
+#'  An object of class OM
+#'
+"WM_Wenchman_OM"
+
+
+#'  YM_YellowmouthGrouper_OM OM
+#'
+#'  An object of class OM
+#'
+"YM_YellowmouthGrouper_OM"
+
+
+#'  Swordfish_OM OM
+#'
+#'  An object of class OM
+#'  Author: Alex Hanke (DFO)  Assessment: http://iccat.org/Documents/SCRS/Manual/CH2/2_1_9_SWO_ENG.pdf
+#' 
+"Swordfish_OM"
+
+
+#'  Arrowtooth_Flounder_BC_SS OM
+#'
+#'  An object of class OM (from iSCAM using iSCAM2DLM)
+#'  DFO. Author: DFO.
+#' 
+"Arrowtooth_Flounder_BC_SS"
+
+
+#'  Rougheye_Rockfish_BC_SRA OM
+#'
+#'  An object of class OM (built using StochasticSRA)
+#'  Stock assessment for Canary rockfish (Sebastes pinniger) in British Columbia waters. Stanley R. Starr P. Olsen N. 
+Sustainable Harvest Levels of Anadromous Arctic Char
+from the Hornaday River, Northwest Territories
+#' 
+"Rougheye_Rockfish_BC_SRA"
+
+
